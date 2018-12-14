2074058e2aa765bbde1a0e0610491029a682fecb
Dubois Matthieu <dubois.matthieu@live.be>
2018-12-15 00:47:04 +0100

    Mise en place de graphql/prisma

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,3 +1,20 @@
-const mutations = {};
+const Mutations = {
+    createItem(parent, args, ctx, info){
+        // Check if they are logged in
+        const item = ctx.db.mutation.createItem({
+            data: {
+                ...args
+            }
+        }, info)
+        return item
+    }
+    /* createDog(parent, args, ctx, info){
+        // Create Dog !
+        global.dogs = global.dogs || []
+        const newDog = { name: args.name }
+        global.dogs.push(newDog)
+        return newDog
+    } */
+};
 
-module.exports = mutations;
+module.exports = Mutations;
