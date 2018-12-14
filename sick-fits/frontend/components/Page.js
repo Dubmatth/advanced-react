3bebfad87596ff197120f9049430f7640bc906f0
Dubois Matthieu <dubois.matthieu@live.be>
2018-12-14 09:19:46 +0100

    Petites modifs

--- sick-fits/frontend/components/Page.js
+++ sick-fits/frontend/components/Page.js
@@ -1,6 +1,13 @@
 import React, { Component } from 'react'
 import Header from './Header'
 import Meta from './Meta'
+import styled from 'styled-components'
+
+const MyButton = styled.button`
+  background-color: seagreen;
+  color: white;
+  font-size: 100px;
+`
 
 export default class Page extends Component {
   render() {
@@ -8,6 +15,7 @@ export default class Page extends Component {
       <div>
         <Meta />
         <Header />
+        <MyButton>Click me !</MyButton>
         {this.props.children}
       </div>
     )
